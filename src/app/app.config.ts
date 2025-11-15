ab17da7adc89424a092b73d4970aa502a3fc18ea
Pavel Pogorelov <engineer.pavel@yandex.com>
2025-11-15 23:17:04 +0300

    добавлен prime NG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,20 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import {providePrimeNG} from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ],
 };
